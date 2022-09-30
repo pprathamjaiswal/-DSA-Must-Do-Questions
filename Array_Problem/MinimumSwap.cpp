578e12bc770413b58b46b1908532f1c4baf2dfd5
Pratham Jaiswal <pratham.m.jaiswal@gmail.com>
2022-09-30 21:45:30 +0530

    initial commit

---
+++ Array_Problem/MinimumSwap.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int Minimumswap(int *arr, int n, int k)
+{
+    int count = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] <= k)
+        {
+            count++;
+        }
+    }
+    int bad = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] > k)
+        {
+            bad++;
+        }
+    }
+    int ans = bad;
+    for (int i = 0, j = count; j < n; i++, j++)
+    {
+        if (arr[i] > k)
+        {
+            bad--;
+        }
+        if (arr[j] > k)
+        {
+            bad++;
+        }
+        ans = min(ans, bad);
+    }
+    return ans;
+}
+
+int main()
+{
+    int arr[] = {2, 1, 5, 6, 3};
+    cout << Minimumswap(arr, 5, 3) << endl;
+}
