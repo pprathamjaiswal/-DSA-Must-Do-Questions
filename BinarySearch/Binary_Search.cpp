642c6fdc9e444851d0ccefb7c9c85e134dae9f41
Pratham Jaiswal <pratham.m.jaiswal@gmail.com>
2022-10-30 01:29:06 +0530

    Binary_Search Intro

---
+++ BinarySearch/Binary_Search.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+int Search(int arr[], int size, int target)
+{
+    int start = 0;
+    int end = size - 1;
+
+    while (start <= end)
+    {
+        int mid = start + (end - start) / 2;
+        if (arr[mid] == target)
+        {
+            return mid;
+        }
+
+        if (arr[mid] > target)
+        {
+            end = mid - 1;
+        }
+        else
+        {
+            start = mid + 1;
+        }
+        mid = start + (end - start) / 2;
+    }
+    return -1;
+}
+
+int main()
+{
+    int arr[] = {2, 4, 6, 8, 9, 11, 23};
+    int n = 7;
+    int target = 2;
+    cout << "Index is  "<< Search(arr, n, target) << endl;
+
+    return 0;
+}
