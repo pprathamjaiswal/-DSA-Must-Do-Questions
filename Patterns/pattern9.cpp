af56da429c55c5bc8d7a7d8a5dd4826bf3bb1463
Pratham Jaiswal <pratham.m.jaiswal@gmail.com>
2023-01-17 13:15:23 +0530

    Patterns Problems

---
+++ Patterns/pattern9.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    int row = 1;
+    int value = row;
+    while (row <= n)
+    {
+        int col = 1;
+        while (col <= row)
+        {
+            cout << value++<<" ";
+            col++;
+        }
+        cout << endl;
+        row++;
+    }
+}
