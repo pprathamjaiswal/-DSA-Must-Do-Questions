19124b6a105ca2eb63d1a3132d2165bca68bdb33
Pratham Jaiswal <pratham.m.jaiswal@gmail.com>
2023-01-19 17:29:42 +0530

    Number of 1 Bits LeetCode Problems

---
+++ Array_Problem/Numberofbits.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+// LeetCode Problem
+int hammingWeight(uint32_t n)
+{
+    int count = 0;
+    while (n != 0)
+    {
+        if (n & 1)
+        {
+            count++;
+        }
+        n = n >> 1;
+    }
+    return count;
+}
+int main()
+{
+    int n;
+    cin >> n;
+    cout << hammingWeight(n);
+}
